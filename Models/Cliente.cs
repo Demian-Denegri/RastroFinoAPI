ec0ad19a53241fe61152b6b6569196ad1cac9dc9
Denegri Demian <102878081+Demian-Denegri@users.noreply.github.com>
2026-07-18 15:07:09 -0300

    Fix modelo Cliente

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -6,16 +6,10 @@ namespace RastroFinoAPI.Models
     {
         [Key]
         public int IdCliente { get; set; }
-       public string Nombre { get; set; }
-
-       public string Apellido { get; set; }
-
-       public string Email { get; set; }
-
-       public string Telefono { get; set; }
-       public string Direccion { get; set; }
-
-
-
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string? Email { get; set; }
+        public string Telefono { get; set; }
+        public string? Direccion { get; set; }
     }
 }
